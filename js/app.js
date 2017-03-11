ec3263bbb6f4886c198601991a27b94d95b6cbfe
Ya-Nan Chen <1756538768@qq.com>
2017-03-11 23:43:37 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -86,7 +86,7 @@ particlesJS('particles-js',
       "events": {
         "onhover": {
           "enable": true,
-          "mode": "grab"
+          "mode": "repulse"
         },
         "onclick": {
           "enable": true,
